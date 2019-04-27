83b69a2254fca6d57691b9178b25647d47a8dfc8
16jmiller <16jamill16@gmail.com>
2019-04-27 13:39:33 -0700

    add changes to controller

--- ArchitectureAlloySpecGenerator/Controllers/HomeController.cs
+++ ArchitectureAlloySpecGenerator/Controllers/HomeController.cs
@@ -13,5 +13,25 @@ namespace ArchitectureAlloySpecGenerator.Controllers
         {
             return View();
         }
+
+        public ActionResult Submit()
+        {
+            //Determine from JSON which system is built
+            //If clientServer
+            ClientServerSystemModel system = new ClientServerSystemModel();
+
+            //If PipeFilter
+            PipeFilterSystemModel system = new PipeFilterSystemModel();
+
+            StringBuilder spec = new StringBuilder();
+            // Abbie will create the system from Trent's json object here:
+
+            // If Client Server:
+            spec = CnsSpecCreater.CreateSpec(system);
+
+            // Abbie will output the spec to the file here:
+
+            return View();
+        }
     }
 }
