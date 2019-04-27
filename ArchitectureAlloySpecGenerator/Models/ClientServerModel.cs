3d35093eff44b218758ed0cb8fb9cece830f3bb2
16jmiller <16jamill16@gmail.com>
2019-04-27 13:33:31 -0700

    merge in models, implementations, and interfaces.

---
+++ ArchitectureAlloySpecGenerator/Models/ClientServerModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArchitectureAlloySpecGenerator.Models
+{
+    public class CnsComponent
+    {
+        public string Name { get; set; }
+    }
+
+    public class CnsConnector
+    {
+        public string Name { get; set; }
+        public List<Requester> Requesters { get; set; }
+        public List<Provider> Providers { get; set; }
+    }
+
+    public class CnsPort
+    {
+        public string Name { get; set; }
+    }
+
+    public class CnsRole
+    {
+        public string Name { get; set; }
+    }
+
+    public class ClientServerSystemModel
+    {
+        public string Name { get; set; }
+        public List<CnsConnector> Connectors { get; set; }
+        public List<CnsServer> Servers { get; set; }
+        public List<CnsClient> Clients { get; set; }
+        public Dictionary<CnsRole, CnsPort> Attachments { get; set; }
+    }
+
+    public class CnsServer : CnsComponent
+    {
+        public List<ClientAccess> ClientAccesses { get; set; }
+    }
+
+    public class CnsClient : CnsComponent
+    {
+        public List<CnsServerRequest> ServerRequests { get; set; }
+    }
+
+    public class CnsClientAccess : CnsPort { }
+
+    public class CnsProvider : CnsRole { }
+
+    public class CnsRequester : CnsRole { }
+
+    public class CnsServerRequest : CnsPort { }
+
+}
---
+++ ArchitectureAlloySpecGenerator/Models/PipeFilterModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArchitectureAlloySpecGenerator.Models
+{
+    public class Component
+    {
+        public string Name { get; set; }
+    }
+
+    public class PnfPipe
+    {
+        public string Name { get; set; }
+        public List<PnfSink> Sinks { get; set; }
+        public List<PnfSource> Sources { get; set; }
+    }
+
+    public class PnfPort
+    {
+        public string Name { get; set; }
+    }
+
+    public class PnfRole
+    {
+        public string Name { get; set; }
+    }
+
+    public class PipeFilterSystemModel
+    {
+        public string Name { get; set; }
+        public List<PnfDataSink> DataSinks { get; set; }
+        public List<PnfDataSource> DataSources { get; set; }
+        public List<PnfFilter> Filters { get; set; }
+        public List<PnfPipe> Pipes { get; set; }
+        public Dictionary<PnfRole, PnfPort> Attachments { get; set; }
+    }
+
+    public class PnfFilter : PnfComponent
+    {
+        public List<PnfInput> Inputs { get; set; }
+        public List<PnfOutput> Outputs { get; set; }
+    }
+
+    public class PnfDataSource : PnfComponent
+    {
+        public List<PnfOutput> Outputs { get; set; }
+    }
+
+    public class PnfDataSink : PnfComponent
+    {
+        public List<PnfInput> Inputs { get; set; }
+    }
+
+    public class PnfInput : PnfPort { }
+
+    public class PnfSink : PnfRole { }
+
+    public class PnfSource : PnfRole { }
+
+    public class PnfOutput : PnfPort { }
+
+}
